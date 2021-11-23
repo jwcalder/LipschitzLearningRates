a29cbafa39b10a6ef6face990b80969f6158261a
Jeff Calder <jcalder@umn.edu>
2021-11-22 19:41:57 -0600

    Experimental results update

--- all_experiments.sh
+++ all_experiments.sh
@@ -15,54 +15,53 @@
 #   -c (--num_cores=): Number of cores to use in parallel processing (default=1).
 
 #All experiments with iid sequence
-python3 aronsson_experiment.py -D neumann_star -b 2.5,1               -t 100 -p -c 20     | tee results/aronsson_star_gscale1.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.8,0.6666          -t 100 -p -c 20     | tee results/aronsson_star_gscale2.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.5,0.5             -t 100 -p -c 20     | tee results/aronsson_star_gscale3.csv
+#python3 aronsson_experiment.py -D neumann_star -b 3,1                 -t 100 -p -c 20     | tee results/aronsson_star_gscale1.csv
+#python3 aronsson_experiment.py -D neumann_star -b 1.1,0.6666          -t 100 -p -c 20     | tee results/aronsson_star_gscale2.csv
+#python3 aronsson_experiment.py -D neumann_star -b 0.65,0.5            -t 100 -p -c 20     | tee results/aronsson_star_gscale3.csv
 
-python3 aronsson_experiment.py -D neumann_star -b 2.5,1        -s     -t 20  -p -c 20     | tee results/aronsson_star_singular_gscale1.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.8,0.6666   -s     -t 20  -p -c 20     | tee results/aronsson_star_singular_gscale2.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.5,0.5      -s     -t 20  -p -c 20     | tee results/aronsson_star_singular_gscale3.csv
+python3 aronsson_experiment.py -D neumann_star -b 3,1          -d     -t 100 -p -c 20     | tee results/aronsson_star_dilateBC_gscale1.csv
+python3 aronsson_experiment.py -D neumann_star -b 1.1,0.6666   -d     -t 100 -p -c 20     | tee results/aronsson_star_dilateBC_gscale2.csv
+python3 aronsson_experiment.py -D neumann_star -b 0.65,0.5     -d     -t 100 -p -c 20     | tee results/aronsson_star_dilateBC_gscale3.csv
 
-python3 aronsson_experiment.py -D neumann_star -b 2.5,1        -d     -t 100 -p -c 20     | tee results/aronsson_star_dilateBC_gscale1.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.8,0.6666   -d     -t 100 -p -c 20     | tee results/aronsson_star_dilateBC_gscale2.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.5,0.5      -d     -t 100 -p -c 20     | tee results/aronsson_star_dilateBC_gscale3.csv
+python3 aronsson_experiment.py -D square       -b 3,1                 -t 100 -p -c 20     | tee results/aronsson_square_gscale1.csv
+python3 aronsson_experiment.py -D square       -b 1.1,0.6666          -t 100 -p -c 20     | tee results/aronsson_square_gscale2.csv
+python3 aronsson_experiment.py -D square       -b 0.65,0.5            -t 100 -p -c 20     | tee results/aronsson_square_gscale3.csv
 
-python3 aronsson_experiment.py -D neumann_star -b 2.5,1        -d -s  -t 20  -p -c 20     | tee results/aronsson_star_dilateBC_singular_gscale1.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.8,0.6666   -d -s  -t 20  -p -c 20     | tee results/aronsson_star_dilateBC_singular_gscale2.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.5,0.5      -d -s  -t 20  -p -c 20     | tee results/aronsson_star_dilateBC_singular_gscale3.csv
+python3 aronsson_experiment.py -D square       -b 3,1             -s  -t 20  -p -c 20     | tee results/aronsson_square_singular_gscale1.csv
+python3 aronsson_experiment.py -D square       -b 1.1,0.6666      -s  -t 20  -p -c 20     | tee results/aronsson_square_singular_gscale2.csv
+python3 aronsson_experiment.py -D square       -b 0.65,0.5        -s  -t 20  -p -c 20     | tee results/aronsson_square_singular_gscale3.csv
 
-python3 aronsson_experiment.py -D square       -b 2.5,1               -t 100 -p -c 20     | tee results/aronsson_square_gscale1.csv
-python3 aronsson_experiment.py -D square       -b 0.8,0.6666          -t 100 -p -c 20     | tee results/aronsson_square_gscale2.csv
-python3 aronsson_experiment.py -D square       -b 0.5,0.5             -t 100 -p -c 20     | tee results/aronsson_square_gscale3.csv
+python3 aronsson_experiment.py -D neumann_star -b 3,1          -s     -t 20  -p -c 20     | tee results/aronsson_star_singular_gscale1.csv
+python3 aronsson_experiment.py -D neumann_star -b 1.1,0.6666   -s     -t 20  -p -c 20     | tee results/aronsson_star_singular_gscale2.csv
+python3 aronsson_experiment.py -D neumann_star -b 0.65,0.5     -s     -t 20  -p -c 20     | tee results/aronsson_star_singular_gscale3.csv
 
-python3 aronsson_experiment.py -D square       -b 2.5,1           -s  -t 20  -p -c 20     | tee results/aronsson_square_singular_gscale1.csv
-python3 aronsson_experiment.py -D square       -b 0.8,0.6666      -s  -t 20  -p -c 20     | tee results/aronsson_square_singular_gscale2.csv
-python3 aronsson_experiment.py -D square       -b 0.5,0.5         -s  -t 20  -p -c 20     | tee results/aronsson_square_singular_gscale3.csv
-
+python3 aronsson_experiment.py -D neumann_star -b 3,1          -d -s  -t 20  -p -c 20     | tee results/aronsson_star_dilateBC_singular_gscale1.csv
+python3 aronsson_experiment.py -D neumann_star -b 1.1,0.6666   -d -s  -t 20  -p -c 20     | tee results/aronsson_star_dilateBC_singular_gscale2.csv
+python3 aronsson_experiment.py -D neumann_star -b 0.65,0.5     -d -s  -t 20  -p -c 20     | tee results/aronsson_star_dilateBC_singular_gscale3.csv
 
 #All experiments with grid
-python3 aronsson_experiment.py -D neumann_star -b 2.5,1      -g       -t 1                | tee results/aronsson_grid_star_gscale1.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.8,0.6666 -g       -t 1                | tee results/aronsson_grid_star_gscale2.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.5,0.5    -g       -t 1                | tee results/aronsson_grid_star_gscale3.csv
+python3 aronsson_experiment.py -D neumann_star -b 3,1        -g       -t 1                | tee results/aronsson_grid_star_gscale1.csv
+python3 aronsson_experiment.py -D neumann_star -b 1.1,0.6666 -g       -t 1                | tee results/aronsson_grid_star_gscale2.csv
+python3 aronsson_experiment.py -D neumann_star -b 0.65,0.5   -g       -t 1                | tee results/aronsson_grid_star_gscale3.csv
 
-python3 aronsson_experiment.py -D neumann_star -b 2.5,1      -g -s    -t 1                | tee results/aronsson_grid_star_singular_gscale1.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.8,0.6666 -g -s    -t 1                | tee results/aronsson_grid_star_singular_gscale2.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.5,0.5    -g -s    -t 1                | tee results/aronsson_grid_star_singular_gscale3.csv
+python3 aronsson_experiment.py -D neumann_star -b 3,1        -g -s    -t 1                | tee results/aronsson_grid_star_singular_gscale1.csv
+python3 aronsson_experiment.py -D neumann_star -b 1.1,0.6666 -g -s    -t 1                | tee results/aronsson_grid_star_singular_gscale2.csv
+python3 aronsson_experiment.py -D neumann_star -b 0.65,0.5   -g -s    -t 1                | tee results/aronsson_grid_star_singular_gscale3.csv
 
-python3 aronsson_experiment.py -D neumann_star -b 2.5,1      -g -d    -t 1                | tee results/aronsson_grid_star_dilateBC_gscale1.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.8,0.6666 -g -d    -t 1                | tee results/aronsson_grid_star_dilateBC_gscale2.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.5,0.5    -g -d    -t 1                | tee results/aronsson_grid_star_dilateBC_gscale3.csv
+python3 aronsson_experiment.py -D neumann_star -b 3,1        -g -d    -t 1                | tee results/aronsson_grid_star_dilateBC_gscale1.csv
+python3 aronsson_experiment.py -D neumann_star -b 1.1,0.6666 -g -d    -t 1                | tee results/aronsson_grid_star_dilateBC_gscale2.csv
+python3 aronsson_experiment.py -D neumann_star -b 0.65,0.5   -g -d    -t 1                | tee results/aronsson_grid_star_dilateBC_gscale3.csv
 
-python3 aronsson_experiment.py -D neumann_star -b 2.5,1      -g -d -s -t 1                | tee results/aronsson_grid_star_dilateBC_singular_gscale1.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.8,0.6666 -g -d -s -t 1                | tee results/aronsson_grid_star_dilateBC_singular_gscale2.csv
-python3 aronsson_experiment.py -D neumann_star -b 0.5,0.5    -g -d -s -t 1                | tee results/aronsson_grid_star_dilateBC_singular_gscale3.csv
+python3 aronsson_experiment.py -D neumann_star -b 3,1        -g -d -s -t 1                | tee results/aronsson_grid_star_dilateBC_singular_gscale1.csv
+python3 aronsson_experiment.py -D neumann_star -b 1.1,0.6666 -g -d -s -t 1                | tee results/aronsson_grid_star_dilateBC_singular_gscale2.csv
+python3 aronsson_experiment.py -D neumann_star -b 0.65,0.5   -g -d -s -t 1                | tee results/aronsson_grid_star_dilateBC_singular_gscale3.csv
 
-python3 aronsson_experiment.py -D square       -b 2.5,1      -g       -t 1            	  | tee results/aronsson_grid_square_gscale1.csv
-python3 aronsson_experiment.py -D square       -b 0.8,0.6666 -g       -t 1            	  | tee results/aronsson_grid_square_gscale2.csv
-python3 aronsson_experiment.py -D square       -b 0.5,0.5    -g       -t 1                | tee results/aronsson_grid_square_gscale3.csv
+python3 aronsson_experiment.py -D square       -b 3,1        -g       -t 1            	  | tee results/aronsson_grid_square_gscale1.csv
+python3 aronsson_experiment.py -D square       -b 1.1,0.6666 -g       -t 1            	  | tee results/aronsson_grid_square_gscale2.csv
+python3 aronsson_experiment.py -D square       -b 0.65,0.5   -g       -t 1                | tee results/aronsson_grid_square_gscale3.csv
 
-python3 aronsson_experiment.py -D square       -b 2.5,1      -g    -s -t 1                | tee results/aronsson_grid_square_singular_gscale1.csv
-python3 aronsson_experiment.py -D square       -b 0.8,0.6666 -g    -s -t 1                | tee results/aronsson_grid_square_singular_gscale2.csv
-python3 aronsson_experiment.py -D square       -b 0.5,0.5    -g    -s -t 1                | tee results/aronsson_grid_square_singular_gscale3.csv
+python3 aronsson_experiment.py -D square       -b 3,1        -g    -s -t 1                | tee results/aronsson_grid_square_singular_gscale1.csv
+python3 aronsson_experiment.py -D square       -b 1.1,0.6666 -g    -s -t 1                | tee results/aronsson_grid_square_singular_gscale2.csv
+python3 aronsson_experiment.py -D square       -b 0.65,0.5   -g    -s -t 1                | tee results/aronsson_grid_square_singular_gscale3.csv
 
 
--- generate_plots.py
+++ generate_plots.py
@@ -1,6 +1,9 @@
 from utils import plotting
 import matplotlib.pyplot as plt
 
-plotting.make_plot('aronsson_star')
+plotting.make_plot('aronsson_star',legend_loc='lower left')
+plotting.make_plot('aronsson_star_dilateBC',legend_loc='upper right')
+plotting.make_plot('aronsson_square',legend_loc='upper right')
+plotting.make_plot('aronsson_square_singular',legend_loc='upper right')
 
 plt.show()
